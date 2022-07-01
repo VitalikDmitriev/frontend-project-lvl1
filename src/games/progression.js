f53429b2f688202108564b7ff4fb9324c41b6301
VitalikDmitriev <vitaliksk99@mail.ru>
2022-07-01 15:39:43 +0300

    fix

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,28 +1,28 @@
-import startGameEngine from '../random.js';
-import randomNumberGenerator from '../index.js';
+import startGameEngine from '../index.js';
+import randomNumberGenerator from '../random.js';
 
 const gameDescription = 'What number is missing in the progression?';
 
 const calculateProgression = () => {
-  const progression = [];
-  const progressionSize = randomNumberGenerator(5, 10);
-  const progressionStep = randomNumberGenerator(1, 5);
-  let progressionElement = 0;
+    const progression = [];
+    const progressionSize = randomNumberGenerator(5, 10);
+    const progressionStep = randomNumberGenerator(1, 5);
+    let progressionElement = 0;
 
-  for (let i = 0; i < progressionSize; i += 1) {
-    progressionElement += progressionStep;
-    progression.push(progressionElement);
-  }
-  return progression;
+    for (let i = 0; i < progressionSize; i += 1) {
+        progressionElement += progressionStep;
+        progression.push(progressionElement);
+    }
+    return progression;
 };
 
 const prepareGameData = () => {
-  const progression = calculateProgression();
-  const hiddenNumber = randomNumberGenerator(1, progression.length - 2);
-  const rightAnswer = String(progression[hiddenNumber]);
-  progression[hiddenNumber] = '..';
-  const question = progression.join(' ');
-  return [question, rightAnswer];
+    const progression = calculateProgression();
+    const hiddenNumber = randomNumberGenerator(1, progression.length - 2);
+    const rightAnswer = String(progression[hiddenNumber]);
+    progression[hiddenNumber] = '..';
+    const question = progression.join(' ');
+    return [question, rightAnswer];
 };
 
 const startProgression = startGameEngine(gameDescription, prepareGameData);
